c5df14d7688fcb4db644a50cafdd599d3905a334
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-17 00:37:13 -0800

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,1 +1,9 @@
 
+const toggleSwitch = document.querySelector('#toggle-theme');
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', '
